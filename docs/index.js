8360a91b689f5c1f4e5a2b23b34026eaee81d240
Ludwing Cano <ludwing238@gmail.com>
2025-02-17 10:14:59 -0600

    adding js content

--- docs/index.js
+++ docs/index.js
@@ -7,10 +7,17 @@ presentations.forEach(presentation => {
     const card = document.createElement('div');
     card.className = 'presentation-card';
     card.innerHTML = `
-            <img src="${presentation.image}" alt="${presentation.title}" width="325" height="200" style="object-fit: cover;">
+            <div style="display: flex; justify-content: center;">
+            <img src="${presentation.image}" alt="${presentation.title}" 
+            width="325" height="200" style="object-fit: cover;">
+            </div>
+            <div>
             <h2>${presentation.title}</h2>
             <p>${presentation.description}</p>
             <a href="${presentation.path}">Ver Presentación</a>
+            </div>
         `;
     grid.appendChild(card);
+
 });
+
--- docs/presentations.js
+++ docs/presentations.js
@@ -1,49 +1,61 @@
 export const presentations = [
     {
         title: "Internet antes de la web",
-        description: "Tema 1",
+        description: "Telnet, Ips, Ports y más",
         path: "temas/internet/index.html",
         image: "temas/internet/images/internet.png"
     },
     {
         title: "Comandos básicos de Linux",
-        description: "Tema 2",
+        description: "Comandos para trabajar en la terminal",
         path: "temas/cli/index.html",
         image: "temas/cli/images/cli.png"
     },
     {
         title: "Introducción a GIT",
-        description: "Tema 3",
+        description: "Control de versiones",
         path: "temas/github/index.html",
         image: "temas/github/images/git.jpg"
     },
     {
         title: "Web Servers",
-        description: "Tema 4",
+        description: "Manejo de servidores web",
         path: "temas/web_servers/index.html",
         image: "temas/web_servers/images/web_server.jpg"
     },
     {
         title: "HTML",
-        description: "Tema 5",
+        description: "Creación de páginas web",
         path: "temas/html/index.html",
         image: "temas/html/images/html.jpeg"
     },
     {
-        title: "SCP",
-        description: "Tema 6",
-        path: "temas/scp/index.html",
-        image: "temas/scp/images/rsync-scp.jpeg"
-    },
-    {
         title: "CSS",
-        description: "Tema 7",
+        description: "Estilos para páginas web",
         path: "temas/css/index.html",
         image: "temas/css/images/css.png"
     },
     {
+        title: "JavaScript",
+        description: "Interactividad en páginas web",
+        path: "temas/js/index.html",
+        image: "temas/js/images/que_es_js.jpg"
+    },
+    {
+        title: "Funciones JavaScript",
+        description: "JavaScript avanzado",
+        path: "temas/js/js_functions/index.html",
+        image: "temas/js/images/js_avanzado.png"
+    },
+    {
+        title: "SCP",
+        description: "Copiar archivos de forma segura",
+        path: "temas/scp/index.html",
+        image: "temas/scp/images/rsync-scp.jpeg"
+    },
+    {
         title: "IA Generativa",
-        description: "Tema extra",
+        description: "Creación de código con IA",
         path: "temas/ia_code/index.html",
         image: "temas/ia_code/images/ia_code.jpeg"
     }
